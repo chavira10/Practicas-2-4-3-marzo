0c0902217ead5ed1da46521a51275c6e32cd26cc
chavira10 <60410366+chavira10@users.noreply.github.com>
2020-03-03 14:39:01 -0800

    Update Hola.s

--- Hola.s
+++ Hola.s
@@ -1,1 +1,11 @@
+.data
+var1:             .word    3
+puntero_var1:    .word    var1
 
+.text
+.global  main
+main:    ldr      r0, =puntero_var1
+         ldr      r1, [r0]
+         ldr      r2, [r1]
+         ldr      r3, =var1
+         bx       lr
